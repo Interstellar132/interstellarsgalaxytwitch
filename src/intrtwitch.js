cc576f5b59e317ff605cb5b1cd14290070c17b2e
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2026-01-04 00:08:56 -0800

    Update intrtwitch.js

--- src/intrtwitch.js
+++ src/intrtwitch.js
@@ -1,1 +1,15 @@
+require("dotenv").config();
+const tmi = require("tmi.js");
+const config = require("../config/glxyconfig.json");
+
+const client = new tmi.Client({
+  options: { debug: true },
+  identity: {
+    username: config.username,
+    password: process.env.TWITCH_OAUTH
+  },
+  channels: [config.channel]
+});
+
+client.connect();
 
