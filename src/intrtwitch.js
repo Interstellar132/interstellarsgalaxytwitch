bfcb11ddb5e0e6b78675e895a4a553e7561962c6
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2026-01-04 00:17:35 -0800

    Update intrtwitch.js

--- src/intrtwitch.js
+++ src/intrtwitch.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const tmi = require("tmi.js");
 const config = require("../config/glxyconfig.json");
+const bannedWordsCheck = require("./moderation/bannedWords");
 
 const client = new tmi.Client({
   options: { debug: true },
@@ -13,3 +14,10 @@ const client = new tmi.Client({
 
 client.connect();
 
+client.on("message", (channel, tags, message, self) => {
+  if (self) return;
+
+  const user = tags.username;
+
+  bannedWordsCheck(client, channel, user, message, config.timeout);
+});
